9713737504f3a86deee70019133c5b01bfeeb233
ryusei172525 <ryusei172525@gmail.com>
2024-07-02 01:02:03 +0900

    add clusters papaers of weak lensing and monte carlo

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ const Home: React.FC = () => {
         <li><Link href="/quiz?category=observations" className="block"><div className="card">観測とデータクイズ</div></Link></li>
         <li><Link href="/quiz?category=applied" className="block"><div className="card">応用クイズ</div></Link></li>
         <li><Link href="/quiz?category=sed_dust" className="block"><div className="card">SEDとダスト進化クイズ(Nishida et al., 2022)</div></Link></li>
+        <li><Link href="/quiz?category=weak_lensing" className="block"><div className="card">銀河団の弱重力レンズバイアス(Wonki Lee et al., 2023)</div></Link></li>
+        <li><Link href="/quiz?category=clusters_dynamics" className="block"><div className="card">モンテカルロで解く銀河団の力学(Dawson William et al., 2013)</div></Link></li>
+
       </ul>
     </div>
   );
